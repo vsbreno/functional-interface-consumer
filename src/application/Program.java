77f625aa7e7f7faa010237ac273153fc3c56e5c9
vsbreno <brenovinicius.santos0@gmail.com>
2026-03-22 09:03:09 -0300

    Reference method with static method

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(new MyConsumer());
+		list.forEach(Product::myConsumer);
 		
 		list.forEach(System.out::println);
 	}
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -26,6 +26,10 @@ public class Product{
 		this.price = price;
 	}
 	
+	public static void myConsumer (Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
 	@Override
 	public String toString() {
 		return getName() +
