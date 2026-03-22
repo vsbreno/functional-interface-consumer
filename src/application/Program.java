4e7d871acd5a6bbea391ff882037f7ee8f83fbfe
vsbreno <brenovinicius.santos0@gmail.com>
2026-03-22 09:09:16 -0300

    anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import model.entities.Product;
-import model.util.MyConsumer;
 
 public class Program {
 
@@ -19,7 +19,15 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticMyConsumer);
+		Consumer<Product> con = new Consumer<Product>() {
+			@Override
+			public void accept(Product p) {
+				p.setPrice(p.getPrice() * 1.1);
+			}
+			
+		};
+		
+		list.forEach(con);
 		
 		list.forEach(System.out::println);
 	}
